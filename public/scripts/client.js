07fe042452d18cfda9d0e9946c416ae8b8782f7f
Umang Patel <umangpatel1991.cmail.com>
2021-10-06 19:36:56 -0400

    added function to render multiple tweets with proper time for mate of tweet created

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,6 +5,43 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(() => {
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+  const renderTweets = function(tweets) {
+    const $tweetContainer = $("#tweets-container");
+    $tweetContainer.empty();
+
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $tweetContainer.append($tweet);
+    }
+  };
+
   const createTweetElement = (tweet) => {
     const $tweetArticle = $('<article>').addClass('tweet');
 
@@ -27,7 +64,7 @@ $(() => {
 
     const $articleFooter = $('<footer>');
 
-    const $footerTime = $('<span>').addClass('time-passed').text(tweet.created_at);
+    const $footerTime = $('<span>').addClass('time-passed').text(timeago.format(tweet.created_at));
     const $footerIcons = $('<div>').addClass('respond-icons');
     const $flagIcon = $('<i>').addClass('fas fa-flag fa-sm');
     const $retweetIcon = $('<i>').addClass('fas fa-retweet fa-sm');
@@ -41,23 +78,6 @@ $(() => {
   };
 
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  };
-
-  const $tweet = createTweetElement(tweetData);
-
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  renderTweets(data);
 
 });
