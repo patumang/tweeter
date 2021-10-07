74d916e0438574f6181aa578259e2dc08071f8a2
Umang Patel <umangpatel1991.cmail.com>
2021-10-06 20:21:10 -0400

    added function to fetch data from server using Ajax request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,32 +5,25 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(() => {
-  // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
+  // making a get request to see some data
+  const loadTweets = () => {
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+      dataType: "json",
+      success: (tweets) => {
+        console.log("data:", tweets);
+        tweets.reverse();
+        renderTweets(tweets);
+
       },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+      error: (err) => {
+        console.log(`there was an error: ${err}`);
+      }
+    });
+  };
+
+  loadTweets();
 
   const renderTweets = function(tweets) {
     const $tweetContainer = $("#tweets-container");
@@ -77,9 +70,6 @@ $(() => {
     return $tweetArticle;
   };
 
-
-  renderTweets(data);
-
   const $form = $("#new-tweet-form");
   $form.on("submit", function(event) {
     event.preventDefault();
@@ -88,9 +78,9 @@ $(() => {
     const serializedData = $(this).serialize();
 
     console.log(serializedData);
-    $.post("/tweets/", serializedData, (response) => {
+    $.post("/tweets", serializedData, (response) => {
       console.log(response);
-      //fetchBlogs();
+      loadTweets();
     });
   });
 
